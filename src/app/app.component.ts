3498192b60f8cb01a8bfd09757960c0711adfd91
javiergranadosr <javiergranadosrdev@gmail.com>
2022-06-23 15:47:03 -0500

    Cambios en modo dark y light

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,39 +1,38 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-
 export class AppComponent implements OnInit {
-
   title: string = 'todoApp';
   titleMain: string = 'TODO';
   modeLight: boolean = true;
   modeDark: boolean = false;
-  mode: string = "dark";
+  mode: string = 'dark';
 
   ngOnInit(): void {
     this.mode = localStorage.getItem('mode')!;
+    this.changeMode(this.mode);
   }
 
   changeMode(mode: string) {
-    if (mode === 'D') {
-      this.modeDark = false;
+    if (mode === 'dark') {
+      this.mode = 'dark';
       this.modeLight = true;
-      this.mode = "dark";
-      localStorage.setItem('mode', "dark");
-    } else if (mode === 'L') {
+      this.modeDark = false;
+      localStorage.setItem('mode', 'dark');
+    } else if (mode === 'light') {
+      this.mode = 'light';
       this.modeLight = false;
       this.modeDark = true;
-      this.mode = "light";
-      localStorage.setItem('mode', "light");
+      localStorage.setItem('mode', 'light');
     } else {
-      this.modeDark = false;
+      this.mode = 'light';
       this.modeLight = true;
-      this.mode = "light";
-      localStorage.setItem('mode', "light");
+      this.modeDark = false;
+      localStorage.setItem('mode', 'light');
     }
   }
 }
