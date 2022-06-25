ee1c57a32508e8ab6ecd6d2fa5c2002e9abe803e
javiergranadosr <javiergranadosrdev@gmail.com>
2022-06-25 16:47:01 -0500

    Se maqueto el input create

--- src/app/tasks/components/input-create/input-create.component.ts
+++ src/app/tasks/components/input-create/input-create.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-input-create',
   templateUrl: './input-create.component.html',
   styleUrls: ['./input-create.component.css']
 })
-export class InputCreateComponent implements OnInit {
-
+export class InputCreateComponent implements OnInit, DoCheck {
+  mode: string = "";
   constructor() { }
 
+  ngDoCheck(): void {
+    this.mode = localStorage.getItem('mode')!;
+  }
+
   ngOnInit(): void {
+    this.mode = localStorage.getItem('mode')!;
   }
 
 }
