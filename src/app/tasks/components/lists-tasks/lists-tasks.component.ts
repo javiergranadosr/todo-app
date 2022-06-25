015095dad6aba055299bbbaea1926b204be74c21
javiergranadosr <javiergranadosrdev@gmail.com>
2022-06-25 17:31:38 -0500

    Se agrego el maquetado de la lista de tareas

--- src/app/tasks/components/lists-tasks/lists-tasks.component.ts
+++ src/app/tasks/components/lists-tasks/lists-tasks.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-lists-tasks',
   templateUrl: './lists-tasks.component.html',
   styleUrls: ['./lists-tasks.component.css']
 })
-export class ListsTasksComponent implements OnInit {
-
+export class ListsTasksComponent implements OnInit, DoCheck {
+  mode: string = "";
   constructor() { }
 
+  ngDoCheck(): void {
+    this.mode = localStorage.getItem('mode')!;
+  }
+
   ngOnInit(): void {
+    this.mode = localStorage.getItem('mode')!;
   }
 
 }
